f3f2156e58d50be9b54662fb4e1d76c44ff249cd
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-05-19 17:52:09 +1000

    WIP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ev3-track
 Random stuff with ev3
 
-Download https://raw.githubusercontent.com/trevorablett/inputs/non-blocking-gamepad/inputs.py
+Download inputs from me at maxfire2008/inputs
--- control_server.py
+++ control_server.py
@@ -1,15 +1,27 @@
 import flask
 import json
 import inputs
+import time
 
 app = flask.Flask(__name__)
 
 current_controller_state = {}
+
 def controller_state():
+    start = time.time()
     global current_controller_state
-    for event in inputs.devices.gamepads[0].read():
-        current_controller_state[event.code] = event.state
-    return current_controller_state
+    while True:
+        events = inputs.devices.gamepads[0].read()
+        # print(events)
+        if events:
+            for event in events:
+                print(event.code,event.state)
+                current_controller_state[event.code] = event.state
+        else:
+            break
+    print("Controller state updated in:", time.time() - start)
+    # return current_controller_state
+    return {i:current_controller_state[i] for i in sorted(current_controller_state)}
 
 @app.route("/CONTROLLER_STATE", methods=["GET"])
 def get_controller_state():
